fc6ed0939fd430d92ccef8111657c19f38fc2cc1
Jufino <jufo@atlas.sk>
2011-10-31 11:49:03 -0500

    Beagle <-> PC = OK

--- protocol.cpp
+++ protocol.cpp
@@ -8,46 +8,35 @@ using namespace std;
 #define char_for_array_serial 10
 #include "libprotocol.c";
 #include "libsocket.c";
-#include "libserial.c"; 
 #include "libspec.c";
 //-----------------------------------------------------------
 int main(void)
 {
-
 //--------Vytvor server a pripoj-----------------------------
-  port_int = open_serial("/dev/ttyO2",B115200); 
-  printf("Serial port open - /dev/ttyO2\n");
-  vytvor_server();
+	vytvor_server();
 //-----------------------------------------------------------
-  char prijemx[40][char_for_array];   //pole pre prijem dat 
-  char prijem_serial[40][char_for_array];   //pole pre prijem dat
+	char data_prijem[40][char_for_array];   //pole pre prijem dat 
+	char data_odosli[40][char_for_array];   //pole pre odosielanie
 //-------mazanie polii---------------------------------------
-	for (int o=0;o<sizeof(prijemx)/sizeof(prijemx[0]);o++){
-		memset(&prijemx[o], 0, sizeof(prijemx[o])); 
+	for (int o=0;o<sizeof(data_prijem)/sizeof(data_prijem[0]);o++){
+		memset(&data_prijem[o], 0, sizeof(data_prijem[o])); 
 	}  
-	for (int o=0;o<sizeof(prijem_serial)/sizeof(prijem_serial[0]);o++){
-		memset(&prijem_serial[o], 0, sizeof(prijem_serial[o])); 
+	for (int o=0;o<sizeof(data_odosli)/sizeof(data_odosli[0]);o++){
+		memset(&data_odosli[o], 0, sizeof(data_odosli[o])); 
 	}  
 //-----------------------------------------------------------
-  get_data_socket(prijemx);     //data zo socket
+	get_data_socket(data_prijem);     //data zo socket
 //-----------------------------------------------------------
 //-----------------------------------------------------------
-  if (strcmp(prijemx[0], "data") == 0){
-    send_data_serial(prijemx,2);
-    receive_data_serial(prijem_serial);
-    send_data_socket(prijem_serial,15);
-  }
-  else if (strcmp(prijemx[0], "hovo") == 0){
-    precitaj_text(prijemx[1]);
-    printf(prijemx[1]);
-  }
-  else{
-//-----------------------------------------------------------
-    send_data_serial(prijemx,2);
-  }
+	if (strcmp(data_prijem[0], "data") == 0){
+//napriklad chceme poslat 15 dat - vlozime si ich do pola
+		for(int q=0;q <15;q++){
+			sprintf(data_odosli[q],"%d",q);
+		}	
+//------------------------------------
+		send_data_socket(data_odosli,15);
+	}
 //---------------------------------------------------
-
-//-----------------------------------------------------------
 	return 0;
 }
 
